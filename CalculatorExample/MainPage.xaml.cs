12a9a9dcd4010870d1032df6a9db2498abcc6f84
Eduardo Rosas <eduardorosas@MacBook-Pro-de-Eduardo.local>
2019-01-17 14:44:47 -0700

    ejemplo de calculadora completo

--- CalculatorExample/MainPage.xaml.cs
+++ CalculatorExample/MainPage.xaml.cs
@@ -9,9 +9,38 @@ namespace CalculatorExample
 {
     public partial class MainPage : ContentPage
     {
+        private bool isDecimal = false;
         public MainPage()
         {
             InitializeComponent();
+
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+        }
+
+        private void NumberButton_Clicked(object sender, EventArgs args)
+        {
+            string currentNumber = resultLabel.Text;
+            string number = (sender as Button).Text;
+            if (!isDecimal)
+            {
+                string newNumber = $"{currentNumber}{number}";
+                float result;
+                if (float.TryParse(newNumber, out result))
+                    resultLabel.Text = result.ToString();
+            }
+            else
+            {
+                string newNumber = $"{currentNumber}.{number}";
+                float result;
+                if (float.TryParse(newNumber, out result))
+                    resultLabel.Text = result.ToString();
+                isDecimal = false;
+            }
+        }
+
+        private void DecimalButton_Clicked(object sender, EventArgs args)
+        {
+            isDecimal = true;
         }
     }
 }
